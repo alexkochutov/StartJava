c022c312f1a163be1f79d8d1a74e1366426a09e9
Aleksandr Kochutov <alex.kochutov@gmail.com>
2023-03-08 16:25:40 +0400

    fix: Add fixes round 3 for Task 3 (Guess Number) of final HW in Lesson 2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -5,14 +5,13 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private int secret;
-    private boolean isGuessed = false;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
         System.out.println("Игроки " + player1.getName() + " и "
                 + player2.getName() + " начинают игру...");
-        this.secret = 1 + (int) (Math.random() * 100);
+        secret = 1 + (int) (Math.random() * 100);
         System.out.println("Компьютер загадал число в полуинтервале (0, 100]");
     }
 
@@ -21,8 +20,7 @@ public class GuessNumber {
             inputNumber(player1);
             if (compareNumbers(player1)) break;
             inputNumber(player2);
-            if (compareNumbers(player2)) break;
-        } while (!isGuessed);
+        } while (!compareNumbers(player2));
     }
 
     private void inputNumber(Player player) {
@@ -35,7 +33,7 @@ public class GuessNumber {
         int number = player.getNumber();
         if (number == secret) {
             System.out.println("Вы угадали!!! Игрок " + player.getName() + " выиграл!");
-            isGuessed = true;
+            return true;
         } else {
             System.out.print("Число " + number);
             if (number > secret) {
@@ -45,6 +43,6 @@ public class GuessNumber {
             }
             System.out.println("того, что загадал компьютер");
         }
-        return isGuessed;
+        return false;
     }
 }
